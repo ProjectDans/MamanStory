cc2447a497810d5db46a2991d812717655c96054
DansCoding22 <gspdifpilangsariendah@gmail.com>
2022-04-22 03:25:14 +0700

    menambahkan fitur log out

--- app/src/main/java/com/danscoding/mamanstory/SnapStoryActivity.kt
+++ app/src/main/java/com/danscoding/mamanstory/SnapStoryActivity.kt
@@ -1,11 +1,27 @@
 package com.danscoding.mamanstory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.danscoding.mamanstory.databinding.ActivitySnapStoryBinding
+import com.danscoding.mamanstory.viewmodel.TokenViewModel
 
 class SnapStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySnapStoryBinding
+    private lateinit var tokenViewModel: TokenViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_snap_story)
+        binding = ActivitySnapStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //aksi untuk log out
+        binding.logoutButton.setOnClickListener{
+            tokenViewModel.removeTokens()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/danscoding/mamanstory/api/ApiConfig.kt
+++ app/src/main/java/com/danscoding/mamanstory/api/ApiConfig.kt
@@ -10,7 +10,7 @@ class ApiConfig {
         fun getApiService(): ApiService {
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
+            val client= OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
