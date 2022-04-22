4684fe2ae9ebf418e7249c64c7ef605d5e1818f2
DansCoding22 <gspdifpilangsariendah@gmail.com>
2022-04-23 02:41:38 +0700

    memperbaiki fitur logout

--- app/src/main/java/com/danscoding/mamanstory/SnapStoryActivity.kt
+++ app/src/main/java/com/danscoding/mamanstory/SnapStoryActivity.kt
@@ -1,20 +1,30 @@
 package com.danscoding.mamanstory
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.danscoding.mamanstory.databinding.ActivitySnapStoryBinding
+import com.danscoding.mamanstory.preference.AccountTokenPreference
 import com.danscoding.mamanstory.viewmodel.TokenViewModel
+import com.danscoding.mamanstory.viewmodel.ViewModelFactory
 
 class SnapStoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySnapStoryBinding
     private lateinit var tokenViewModel: TokenViewModel
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "userToken")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySnapStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val pref = AccountTokenPreference.getInstance(dataStore)
+        tokenViewModel = ViewModelProvider(this, ViewModelFactory(pref))[TokenViewModel::class.java]
 
         //aksi untuk log out
         binding.logoutButton.setOnClickListener{
